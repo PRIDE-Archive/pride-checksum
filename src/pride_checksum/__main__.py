f8a00c5e0d121f00432acb8698ae7fdc2d5d24e5
Chakradhar Bandla <chakrabandla@gmail.com>
2023-11-24 23:10:14 +0000

    support list of files

--- src/pride_checksum/__main__.py
+++ src/pride_checksum/__main__.py
@@ -1,6 +1,11 @@
 import hashlib
 import os
+import pathlib
+import stat
 import sys
+from pathlib import Path
+
+import click as click
 
 
 def sha1sum(filename):
@@ -8,35 +13,68 @@ def sha1sum(filename):
         return hashlib.file_digest(f, 'sha1').hexdigest()
 
 
-def main():
-    files_path = sys.argv[1]
-    if not os.path.isdir(files_path):
-        print("Directory doesn't exists: " + files_path)
+def is_hidden_file(filepath):
+    if os.name == 'nt':
+        return bool(os.stat(filepath).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
+    else:
+        return Path(filepath).name.startswith('.')
+
+
+@click.command()
+@click.option('--files_dir', type=click.Path(), required=False,
+              help="Checksum will be computed for all the files in this directory")
+@click.option('--files_list_path', type=click.Path(), required=False,
+              help="Path of the file that contains list of all files whose Checksum should be computed")
+def main(files_dir, files_list_path):
+    checksum_file = None
+    f_list = []
+    if files_dir is None and files_list_path is None:
+        print("[ERROR] Either dir option or list option should be specified")
         sys.exit(1)
 
-    checksum_file = os.path.join(files_path, "checksum.txt")
-    if os.path.isfile(checksum_file):
-        print("checksum.txt already exists in path:", files_path)
-        yes_no = input("Do you want to overwrite checksum.txt? [y/n]:")
-        if str(yes_no).upper() != 'Y':
-            print("Exiting...")
-            sys.exit(0)
+    if files_dir is not None:
+        if not os.path.isdir(files_dir):
+            print("[ERROR] Directory doesn't exists: " + files_dir)
+            sys.exit(1)
         else:
-            cfile = open(checksum_file, 'w')
-            cfile.write('')
-            cfile.close()
+            checksum_file = os.path.join(files_dir, "checksum.txt")
+            dir_list = os.listdir(files_dir)
+            for filename in dir_list:
+                if filename == 'checksum.txt':
+                    continue
+                f_list.append(os.path.join(files_dir, filename))
 
-    for filename in os.listdir(files_path):
-        if filename == 'checksum.txt':
-            continue
-        f = os.path.join(files_path, filename)
-        if os.path.isfile(f):
+    if files_list_path is not None:
+        if not os.path.isfile(files_list_path):
+            print("[ERROR] File doesn't exists: " + files_list_path)
+            sys.exit(1)
+        else:
+            checksum_file = "checksum.txt"
+            with open(files_list_path, 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    f_list.append(line)
+
+    if os.path.isfile(checksum_file):
+        print("[WARN] checksum.txt already exists in path:", files_dir, "This will be overwritten.")
+        # yes_no = input("Do you want to overwrite checksum.txt? [y/n]:")
+        # if str(yes_no).upper() != 'Y':
+        #     print("Exiting...")
+        #     sys.exit(0)
+        cfile = open(checksum_file, 'w')
+        cfile.write('# SHA-1 Checksum \n')
+        cfile.close()
+
+    for f in f_list:
+        if os.path.isfile(f) and not is_hidden_file(f):
             sha1_sum = sha1sum(f)
             cfile = open(checksum_file, 'a')
-            cfile.write(sha1_sum + '\t' + filename + '\n')
+            cfile.write(f + '\t' + sha1_sum + '\n')
             cfile.close()
+
+    out_path = pathlib.Path(checksum_file).parent.resolve()
+    print("checksum.txt file has been stored in path:", out_path)
 
 
 if __name__ == '__main__':
     main()
-
